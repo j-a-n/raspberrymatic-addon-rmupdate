d853357db97af55ad76d3a9767c8825c62a07c58
Jan Schneider <j.schneider@aktivoptik.de>
2018-01-27 15:51:08 +0100

    Add and use update_addon instead to rely on installed cuxd.

--- pack_addon.sh
+++ pack_addon.sh
@@ -4,7 +4,7 @@ version=$(cat VERSION)
 addon_file="$(pwd)/rmupdate.tar.gz"
 tmp_dir=$(mktemp -d)
 
-for f in VERSION update_script addon ccu1 ccu2 ccurm rmupdate; do
+for f in VERSION update_script update_addon addon ccu1 ccu2 ccurm rmupdate; do
 	[ -e  $f ] && cp -a $f "${tmp_dir}/"
 done
 chmod 755 "${tmp_dir}/update_script"
